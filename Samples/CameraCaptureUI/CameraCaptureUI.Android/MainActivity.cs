bf807a9bd3672e841639ff9b202de856565b2c7b
James Croft <jamz_c@hotmail.co.uk>
2018-10-30 10:49:32 +0000

    Created quick example of usage of Android CameraCaptureUI. Look/feel of samples needs agreeing and fleshing out.

--- Samples/CameraCaptureUI/CameraCaptureUI.Android/MainActivity.cs
+++ Samples/CameraCaptureUI/CameraCaptureUI.Android/MainActivity.cs
@@ -1,19 +1,59 @@
-﻿using Android.App;
-using Android.OS;
-using Android.Support.V7.App;
-using Android.Runtime;
-using Android.Widget;
-
-namespace CameraCaptureUI.Android
+﻿namespace CameraCaptureUI.Android
 {
+    using System;
+
+    using global::Android.App;
+
+    using global::Android.OS;
+
+    using global::Android.Support.V7.App;
+    using global::Android.Widget;
+
+    using XPlat.Media.Capture;
+    using XPlat.Storage;
+
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Button captureImageButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_main);
+            this.SetContentView(Resource.Layout.activity_main);
+        }
+
+        protected override void OnResume()
+        {
+            if (this.captureImageButton != null)
+            {
+                this.captureImageButton.Click -= this.OnCaptureImageClick;
+            }
+
+            base.OnResume();
+
+            this.captureImageButton = this.FindViewById<Button>(Resource.Id.capture_image_button);
+
+            if (this.captureImageButton != null)
+            {
+                this.captureImageButton.Click += this.OnCaptureImageClick;
+            }
+        }
+
+        private async void OnCaptureImageClick(object sender, EventArgs e)
+        {
+            CameraCaptureUI dialog = new CameraCaptureUI(this);
+            dialog.PhotoSettings.MaxResolution = CameraCaptureUIMaxPhotoResolution.HighestAvailable;
+
+            try
+            {
+                IStorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                System.Diagnostics.Debug.WriteLine(file != null ? file.Path : "No image was captured.");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
         }
     }
 }
